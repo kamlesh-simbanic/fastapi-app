143f9cbde42941f519ccdc100e13e6761e771d90
kamlesh thavani <kamlesh.simbanic2022@gmail.com>
2026-04-09 12:30:28 +0530

    ci: fix coverage reporting configuration

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -9,6 +9,7 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const config: Config = {
     coverageProvider: 'v8',
+    coverageReporters: ['json-summary', 'text', 'lcov'],
     testEnvironment: 'jsdom',
     // Add more setup options before each test is run
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
